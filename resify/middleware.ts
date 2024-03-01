ef61491302a305863eec5ff79b69e72480d4c3df
Sakshi Singh <sakshisingh0520@gmail.com>
2024-03-01 15:35:15 -0800

    delete files

--- resify/middleware.ts
+++
@@ -1,9 +0,0 @@
-import { authMiddleware } from "@clerk/nextjs";
-
-export default authMiddleware({
-  publicRoutes: ["/", "/Certificate", "/Certificate/Jx9R4m2NlFb78PqE3iTk"],
-});
-
-export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
-};
